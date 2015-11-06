9c832178476afcb8b47030d7ef08b3dde27577c0
Kneelawk <jedidiah.pommert@gmail.com>
2015-11-06 13:56:07 -0800

    Add mod files

---
+++ src/main/java/org/meelock/collectionofthings/CollectionOfThings.java
@@ -0,0 +1,10 @@
+package org.meelock.collectionofthings;
+
+import org.meelock.collectionofthings.ref.ModRef;
+
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid = ModRef.MOD_ID)
+public class CollectionOfThings {
+
+}
---
+++ src/main/java/org/meelock/collectionofthings/ref/ModRef.java
@@ -0,0 +1,5 @@
+package org.meelock.collectionofthings.ref;
+
+public class ModRef {
+	public static final String MOD_ID = "collectionofthings";
+}
