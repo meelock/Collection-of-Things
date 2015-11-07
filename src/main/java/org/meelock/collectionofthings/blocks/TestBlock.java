ad620961eb30b3d0c23fecbacc66254f33044bb3
meelock <elisha.pommert@gmail.com>
2015-11-06 17:49:42 -0800

    DERPPPPPPPPPPPP

---
+++ src/main/java/org/meelock/collectionofthings/blocks/TestBlock.java
@@ -0,0 +1,13 @@
+package org.meelock.collectionofthings.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class TestBlock extends Block {
+
+	protected TestBlock(Material arg1) {
+		super(arg1);
+		
+	}
+
+}
