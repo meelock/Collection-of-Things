35265558b48f14c18f4a167ae4da878bb99be028
meelock <elisha.pommert@gmail.com>
2015-11-20 18:34:49 -0800

    derp XD

---
+++ src/main/java/org/meelock/collectionofthings/items/machieneactivator1.java
@@ -0,0 +1,12 @@
+package org.meelock.collectionofthings.items;
+
+import net.minecraft.block.material.Material;
+import net.minecraft.item.Item;
+
+public class machieneactivator1 extends Item {
+
+	protected machieneactivator1(Material arg1) {
+		super();
+		
+	}
+}
