80b26a6513fb9f84227000beb5b364da09fbc220
meelock <elisha.pommert@gmail.com>
2015-11-20 18:22:16 -0800

    meh

--- src/main/java/org/meelock/collectionofthings/world/gen/WorldGenArtifactRuin.java
+++ src/main/java/org/meelock/collectionofthings/world/gen/WorldGenArtifactRuin.java
@@ -10,28 +10,13 @@ public class WorldGenArtifactRuin extends WorldGenerator {
 
 	@Override
 	public boolean generate(World par1World, Random par2Random, int par3, int par4, int par5) {
+
 		
-// // layer 1
+// Reference
 //		par1World.setBlock(par3, par4, par5, Blocks.stonebrick);
-//		par1World.setBlock(par3 - 1, par4, par5, Blocks.stonebrick);
-//		par1World.setBlock(par3 - 2, par4, par5, Blocks.stonebrick);
-//		par1World.setBlock(par3, par4, par5 - 1, Blocks.stonebrick);
-//		par1World.setBlock(par3 - 1, par4, par5 - 1, Blocks.stonebrick);
-//		par1World.setBlock(par3 - 2, par4, par5 - 1, Blocks.stonebrick);
-//		par1World.setBlock(par3, par4, par5 - 2, Blocks.stonebrick);
-//		par1World.setBlock(par3 - 1, par4, par5 - 2, Blocks.stonebrick);
-//		par1World.setBlock(par3 - 2, par4, par5 - 2, Blocks.stonebrick);
-// // layer 2
-//		par1World.setBlock(par3, par4 + 1, par5, Blocks.stonebrick);
-//		par1World.setBlock(par3 - 1, par4 + 1, par5, Blocks.stonebrick);
-//		par1World.setBlock(par3 - 2, par4 + 1, par5, Blocks.stonebrick);
-//		par1World.setBlock(par3, par4 + 1, par5 - 1, Blocks.stonebrick);
-//		par1World.setBlock(par3 - 1, par4 + 1, par5 - 1, Blocks.air);
-//		par1World.setBlock(par3 - 2, par4 + 1, par5 - 1, Blocks.stonebrick);
-//		par1World.setBlock(par3, par4 + 1, par5 - 2, Blocks.stonebrick);
-//		par1World.setBlock(par3 - 1, par4 + 1, par5 - 2, Blocks.stonebrick);
-//		par1World.setBlock(par3 - 2, par4 + 1, par5 - 2, Blocks.stonebrick);
-//		System.out.println("generated test structure");
+		
+		
+		
 		
 		
 		
