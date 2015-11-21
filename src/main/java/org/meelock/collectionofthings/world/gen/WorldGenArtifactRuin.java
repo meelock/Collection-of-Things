b3ee2a5e362370e0c844a2cecaec145e3885d84e
meelock <elisha.pommert@gmail.com>
2015-11-20 18:19:41 -0800

    started work on new items, and commented out the test structure.

--- src/main/java/org/meelock/collectionofthings/world/gen/WorldGenArtifactRuin.java
+++ src/main/java/org/meelock/collectionofthings/world/gen/WorldGenArtifactRuin.java
@@ -11,27 +11,31 @@ public class WorldGenArtifactRuin extends WorldGenerator {
 	@Override
 	public boolean generate(World par1World, Random par2Random, int par3, int par4, int par5) {
 		
- // layer 1
-		par1World.setBlock(par3, par4, par5, Blocks.stonebrick);
-		par1World.setBlock(par3 - 1, par4, par5, Blocks.stonebrick);
-		par1World.setBlock(par3 - 2, par4, par5, Blocks.stonebrick);
-		par1World.setBlock(par3, par4, par5 - 1, Blocks.stonebrick);
-		par1World.setBlock(par3 - 1, par4, par5 - 1, Blocks.stonebrick);
-		par1World.setBlock(par3 - 2, par4, par5 - 1, Blocks.stonebrick);
-		par1World.setBlock(par3, par4, par5 - 2, Blocks.stonebrick);
-		par1World.setBlock(par3 - 1, par4, par5 - 2, Blocks.stonebrick);
-		par1World.setBlock(par3 - 2, par4, par5 - 2, Blocks.stonebrick);
- // layer 2
-		par1World.setBlock(par3, par4 + 1, par5, Blocks.stonebrick);
-		par1World.setBlock(par3 - 1, par4 + 1, par5, Blocks.stonebrick);
-		par1World.setBlock(par3 - 2, par4 + 1, par5, Blocks.stonebrick);
-		par1World.setBlock(par3, par4 + 1, par5 - 1, Blocks.stonebrick);
-		par1World.setBlock(par3 - 1, par4 + 1, par5 - 1, Blocks.air);
-		par1World.setBlock(par3 - 2, par4 + 1, par5 - 1, Blocks.stonebrick);
-		par1World.setBlock(par3, par4 + 1, par5 - 2, Blocks.stonebrick);
-		par1World.setBlock(par3 - 1, par4 + 1, par5 - 2, Blocks.stonebrick);
-		par1World.setBlock(par3 - 2, par4 + 1, par5 - 2, Blocks.stonebrick);
-		System.out.println("generated test structure");
+// // layer 1
+//		par1World.setBlock(par3, par4, par5, Blocks.stonebrick);
+//		par1World.setBlock(par3 - 1, par4, par5, Blocks.stonebrick);
+//		par1World.setBlock(par3 - 2, par4, par5, Blocks.stonebrick);
+//		par1World.setBlock(par3, par4, par5 - 1, Blocks.stonebrick);
+//		par1World.setBlock(par3 - 1, par4, par5 - 1, Blocks.stonebrick);
+//		par1World.setBlock(par3 - 2, par4, par5 - 1, Blocks.stonebrick);
+//		par1World.setBlock(par3, par4, par5 - 2, Blocks.stonebrick);
+//		par1World.setBlock(par3 - 1, par4, par5 - 2, Blocks.stonebrick);
+//		par1World.setBlock(par3 - 2, par4, par5 - 2, Blocks.stonebrick);
+// // layer 2
+//		par1World.setBlock(par3, par4 + 1, par5, Blocks.stonebrick);
+//		par1World.setBlock(par3 - 1, par4 + 1, par5, Blocks.stonebrick);
+//		par1World.setBlock(par3 - 2, par4 + 1, par5, Blocks.stonebrick);
+//		par1World.setBlock(par3, par4 + 1, par5 - 1, Blocks.stonebrick);
+//		par1World.setBlock(par3 - 1, par4 + 1, par5 - 1, Blocks.air);
+//		par1World.setBlock(par3 - 2, par4 + 1, par5 - 1, Blocks.stonebrick);
+//		par1World.setBlock(par3, par4 + 1, par5 - 2, Blocks.stonebrick);
+//		par1World.setBlock(par3 - 1, par4 + 1, par5 - 2, Blocks.stonebrick);
+//		par1World.setBlock(par3 - 2, par4 + 1, par5 - 2, Blocks.stonebrick);
+//		System.out.println("generated test structure");
+		
+		
+		
+		
 		return true;
 	}
 
