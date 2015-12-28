5c746edf20ed36bab99c5146a72822066a8d3471
meelock <elisha.pommert@gmail.com>
2015-12-27 20:11:56 -0800

    added set cube util... this might work :P

--- src/main/java/org/meelock/collectionofthings/world/gen/util/SetblockUtil.java
+++ src/main/java/org/meelock/collectionofthings/world/gen/util/SetblockUtil.java
@@ -10,10 +10,25 @@ public class SetblockUtil {
 			for (int y = y1; y1 < y2 ? y <= y2 : y >= y2; y += y1 < y2 ? 1 : -1) {
 				for (int z = z1; z1 < z2 ? z <= z2 : z >= z2; z += z1 < z2 ? 1 : -1) {
 					world.setBlock(x, y, z, block, metadata, notify);
+					}
+				
 				}
-
+		
 			}
 
 		}
+		public static void fillCube(World world, int x1, int y1, int z1, int x2, int y2, int z2, Block block, int metadata,
+				int notify) {
+				
+
+						SetblockUtil.fill(world, x1, y1, z1, x2, y1, z2, block, metadata, notify);
+						SetblockUtil.fill(world, x1, y1, z1, x2, y2, z1, block, metadata, notify);
+						SetblockUtil.fill(world, x1, y1, z1, x1, y2, z2, block, metadata, notify);
+						SetblockUtil.fill(world, x1, y1, z2, x2, y2, z2, block, metadata, notify);
+						SetblockUtil.fill(world, x2, y1, z1, x2, y2, z2, block, metadata, notify);
+						
+						
+						
+
+			}
 	}
-}
