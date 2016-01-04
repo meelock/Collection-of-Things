0cd26af0b9af91cbb3139ef464edd9a4d4d925fb
Meelock <elisha.pommert@gmail.com>
2016-01-03 21:43:09 -0800

    more work on structures :P

--- src/main/java/org/meelock/collectionofthings/world/gen/WorldGenArtifactRuin.java
+++ src/main/java/org/meelock/collectionofthings/world/gen/WorldGenArtifactRuin.java
@@ -10,14 +10,15 @@ import net.minecraft.world.gen.feature.WorldGenerator;
 
 public class WorldGenArtifactRuin extends WorldGenerator {
 
+	// Reference
+	// par1World.setBlock(x, y, z, Blocks.someblock);
 	@Override
 	public boolean generate(World par1World, Random par2Random, int x, int y, int z) {
 
-		
-		
-		
 		SetblockUtil.fillCube(par1World, x, y, z, x + 9, y + 9, z + 9, Blocks.stonebrick, 0, 3);
-
+		SetblockUtil.fill(par1World, x + 1, y + 1, z + 1, x + 8, y + 8, z + 8, Blocks.air, 0, 3);
+		par1World.setBlock(x + 1, y + 1, z + 1, Blocks.stone_slab);
+		SetblockUtil.fill(par1World, x + 4, y + 8, z + 4, x + 5, y + 8, z + 5, Blocks.glowstone, 0, 3);
 
 		return true;
 	}
